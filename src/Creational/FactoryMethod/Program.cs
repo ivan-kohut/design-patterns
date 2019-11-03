266282d2416a4e9d67d9f0497309a321db240cba
Ivan Kohut <kogut.iwan@gmail.com>
2019-11-03 21:46:59 +0200

    Add example of "Factory method"

--- src/Creational/FactoryMethod/Program.cs
+++ src/Creational/FactoryMethod/Program.cs
@@ -6,7 +6,35 @@ namespace FactoryMethod
   {
     static void Main(string[] args)
     {
-      Console.WriteLine("Hello World!");
+      Console.WriteLine("What smartphone do you want to use?");
+
+      Console.WriteLine("Available options:");
+      Console.WriteLine("1 - Apple phone");
+      Console.WriteLine("2 - Samsung phone");
+      Console.WriteLine("3 - Xiaomi phone");
+
+      Console.Write("Your choice: ");
+
+      if (Enum.TryParse(Console.ReadLine(), out CompanyName companyType))
+      {
+        SmartphoneCreator smartphoneCreator = companyType switch
+        {
+          CompanyName.Apple => new AppleSmartphoneCreator(),
+          CompanyName.Samsung => new SamsungSmartphoneCreator(),
+          CompanyName.Xiaomi => new XiaomiSmartphoneCreator(),
+          _ => throw new ArgumentException("Sorry, we don't support such kind of smartphone")
+        };
+
+        Console.Write("Enter a mobile phone: ");
+
+        smartphoneCreator
+          .Create()
+          .Call(Console.ReadLine());
+      }
+      else
+      {
+        Console.WriteLine("Invalid choice. Please try later again.");
+      }
     }
   }
 }
---
+++ src/Creational/FactoryMethod/SmartphoneCreators/SmartphoneCreator.cs
@@ -0,0 +1,7 @@
+﻿namespace FactoryMethod
+{
+  public abstract class SmartphoneCreator
+  {
+    public abstract ISmartphone Create();
+  }
+}
